9e7e69dcde5db2c188a5b8d33c2061603d47fc3c
jiho <jhhj424@naver.com>
2020-08-08 12:03:48 +0900

    구현문제2 - 시각

---
+++ src/Implementation/Implementation_02.java
@@ -0,0 +1,32 @@
+package Implementation;
+
+import java.util.*;
+
+/**
+ * 구현 알고리즘 02 : 시각
+ * 시간복잡도 : O(N)
+ */
+public class Implementation_02 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int cnt = 0;
+
+        for(int i=0; i<=N; i++) {
+            for(int j=0; j<60; j++) {
+                for(int k=0; k<60; k++) {
+                    if(check(i,j,k)) cnt++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    public static boolean check(int h, int m, int s) {
+        if(h % 10 == 3 || m / 10 == 3 || m % 10 == 3 || s / 10 == 3 || s % 10 == 3) {
+            return true;
+        }
+        return false;
+    }
+}
