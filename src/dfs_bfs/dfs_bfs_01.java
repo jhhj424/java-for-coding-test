c6a27246cc8594555d17c51745b1ccbce7964d97
jiho <jhhj424@naver.com>
2020-08-10 23:37:55 +0900

    탐색문제1 - 음료수 얼려 먹기

---
+++ src/dfs_bfs/dfs_bfs_01.java
@@ -0,0 +1,43 @@
+package dfs_bfs;
+
+/**
+ * 탐색 알고리즘 01 : 음료수 얼려 먹기
+ */
+public class dfs_bfs_01 {
+    static int n,m;
+    static int[][] 얼음틀 = {
+            {0,0,1,1,0},
+            {0,0,1,1,1},
+            {1,1,0,1,0},
+            {0,0,1,0,1},
+    };
+    public static void main(String[] args) {
+        n = 얼음틀.length;
+        m = 얼음틀[0].length;
+        System.out.println(음료수얼려먹기(얼음틀));
+    }
+    public static int 음료수얼려먹기(int[][] map) {
+        int ret = 0;
+        for(int i=0; i<map.length; i++) {
+            for(int j=0; j<map[i].length; j++) {
+                if(dfs(i,j) == true) {
+                    ret++;
+                }
+            }
+        }
+        return ret;
+    }
+
+    public static boolean dfs(int x, int y) {
+        if(x<0 || x>n-1 || y<0 || y>m-1) return false;
+        if(얼음틀[x][y] == 0) {
+            얼음틀[x][y] = 1;
+            dfs(x-1, y);
+            dfs(x,y+1);
+            dfs(x+1, y);
+            dfs(x, y-1);
+            return true;
+        }
+        return false;
+    }
+}
