eb0e4226745228b5384992d2b131abb22b82cc76
jiho <jhhj424@naver.com>
2020-08-16 02:17:41 +0900

    이진탐색 - 재귀구현

--- src/binary_tree/binary_search.java
+++ src/binary_tree/binary_search.java
@@ -8,7 +8,7 @@ public class binary_search {
         int n = 10;
         int target = 7;
         int[] arr = {1,3,5,7,9,11,13,15,17,19};
-        int result = binarySearch(arr, target, 0, n-1);
+        int result = binarySearchForWhile(arr, target, 0, n-1);
         if(result == -1) {
             System.out.println("원소가 존재하지 않습니다.");
         } else {
@@ -22,5 +22,15 @@ public class binary_search {
         if(arr[mid] == target)return mid;
         else if(arr[mid] > target) return binarySearch(arr, target, start, mid -1);
         else return binarySearch(arr, target, mid + 1, end);
+    }
+
+    public static int binarySearchForWhile(int[] arr, int target, int start, int end) {
+        while(start <= end) {
+            int mid = (start+end)/2;
+            if(arr[mid] == target) return mid;
+            else if(arr[mid] > target) end = mid-1;
+            else start = mid+1;
+        }
+        return -1;
     }
 }
